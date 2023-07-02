43db2bc25ef8597a0ac8794ba4227652f604febe
I.MaN <jahediman@gmail.com>
2023-07-02 13:19:06 +0330

    Added static files
    + add static file css, bootstrap,...
    + add about page

--- config/urls.py
+++ config/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     # admin
@@ -25,3 +27,5 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('', include('pages.urls')),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- pages/tests.py
+++ pages/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase, SimpleTestCase
 from django.urls import reverse, resolve
 
-from .views import HomePageView
+from .views import HomePageView, AboutPageView
 # Create your tests here.
 class HomePageTest(SimpleTestCase):
     
@@ -22,3 +22,26 @@ class HomePageTest(SimpleTestCase):
         view = resolve('/')
 
         self.assertEqual(view.func.__name__, HomePageView.as_view().__name__)
+        
+        
+        
+class AboutPageTest(SimpleTestCase):
+    def setUp(self) -> None:
+        url = reverse('about')
+        self.response = self.client.get(url)
+
+    def test_url_exists_at_correct_location(self):
+        response = self.client.get('/about')
+        self.assertEqual(response.status_code, 200)
+
+    def test_aboutpage_view(self):
+        self.assertEqual(self.response.status_code, 200)
+        self.assertTemplateUsed(self.response, 'pages/about.html')
+        self.assertContains(self.response, 'About Page')
+        self.assertNotContains(self.response, 'Hi, I should not be their')
+
+    def test_url_resolve_about_page_view(self):
+        view = resolve('/about')
+        self.assertEqual(view.func.__name__ , AboutPageView.as_view().__name__)
+
+    
